5b61233f72155e4c9db84eae54d5b088bee67a21
Kim Enkovaara <kim.enkovaara@iki.fi>
2026-07-20 12:31:07 +0300

    * OOB linux includses packets needed for AI engines, it is smaller to build and no EFI boot stage * OOB linux builds SDK also * Add custom .bif to V++ linker, it does nmt seem to knwo how to parse the full .bif used to link normal bootimage * Add missing sd-card generate to packager

--- vitis/xpfm_creation/build_platform.py
+++ vitis/xpfm_creation/build_platform.py
@@ -15,20 +15,18 @@ rootdir=args.rootdir
 
 print("Rootdir: ",rootdir)
 
-#exit()
-
 client = vitis.create_client()
 client.set_workspace(path=rootdir + "/vitis/build/workspace/")
 
 
 advanced_options = client.create_advanced_options_dict(user_dtsi=rootdir + "/sources/meta-user/recipes-bsp/device-tree/files/system-user-vitis.dtsi",dt_overlay="0",dt_zocl="1")
 
-platform = client.create_platform_component(name = "versal-generic-xcve2302_platform",hw_design = rootdir + "/vivado-hw/vitis_ve2302_platform_2026_1.xsa",os = "linux",cpu = "psv_cortexa72",domain_name = "linux_psv_cortexa72",advanced_options = advanced_options)
+platform = client.create_platform_component(name = "versal-generic-xcve2302_platform",hw_design = rootdir + "/vivado-hw/vitis_ve2302_platform_2026_1.xsa",os = "linux",cpu = "psv_cortexa72",domain_name = "linux_psv_cortexa72",generate_dtb = True,advanced_options = advanced_options)
 
 platform = client.get_component(name="versal-generic-xcve2302_platform")
 domain = platform.get_domain(name="linux_psv_cortexa72")
 
-status = domain.set_bif(path=rootdir + "/build/tmp/deploy/images/versal-generic-xcve2302/boot.bin-extracted/bootgen.bif")
+status = domain.set_bif(path=rootdir + "/vitis/xpfm_creation/boot_custom.bif")
 
 status = domain.set_boot_dir(path=rootdir + "/build/tmp/deploy/images/versal-generic-xcve2302")
 
@@ -37,3 +35,5 @@ status = domain.set_qemu_data(path=rootdir + "/vitis/xpfm_creation/src/a72")
 domain = platform.add_domain(cpu = "ai_engine",os = "aie_runtime",name = "aiengine",display_name = "aiengine")
 
 status = platform.build()
+
+vitis.dispose()
